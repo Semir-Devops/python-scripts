40cdf5bbab8fc29548da6e7cb52e30a3be85cfd0
Semir-Devops <144611511+Semir-Devops@users.noreply.github.com>
2024-04-22 18:34:07 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,59 +1,41 @@
-import os
-import boto3
+import logging
+import json
 import paramiko
-from io import BytesIO
-import base64
+import boto3
+import os
 
 def lambda_handler(event, context):
-    # Get the base64-encoded private key from environment variable
-    base64_encoded_private_key = os.getenv('PRIVATE_KEY')
+    S3Client = boto3.client('s3')
+    bucket_name = "semir-test"
+    S3Client.download_file ('semir-test', 'priv-key/semir-Lambda.pem', '/tmp/keyname.pem')
+    pem_key = paramiko.RSAKey.from_private_key_file("/tmp/keyname.pem")
+    
+    #Create a new client
+    SSH_Client = paramiko.SSHClient()
+    SSH_Client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    host ="54.146.167.126"
+    logging.basicConfig(level=logging.DEBUG)
+    SSH_Client.connect(hostname = host, username = "ec2-user", pkey = pem_key)
+    
+    # Create an SFTP client
+    SFTP_Client = SSHClient.open_sftp()
+    
+    # Path to the source directory on the SFTP server
+    s_path = '/home/ec2-user/source_dir/'
 
-    # Decode the base64-encoded private key
-    private_key = base64.b64decode(base64_encoded_private_key)
+    # Transfer all files from the source directory
+    for filename in SFTP_Client.listdir(s_path):
+        remote_file_path = os.path.join(s_path, filename)
+        local_file_path = '/tmp/' + filename
+        SFTP_Client.get(remote_file_path, local_file_path)
+        s3_client.upload_file(local_file_path, bucket_name, 'sftp-files/' + filename)
+        SFTP_Client.remove(remote_file_path)
 
-    # S3 bucket name and key prefix
-    bucket_name = 'semir-test'
-    s3_key_prefix = 'sftp-files/'
+    # Close the SFTP and SSH connections
+    SFTP_Client.close()
+    SSH_Client.close()
 
-    # Connect to the EC2 instance using paramiko
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-    try:
-        # Load the private key
-        private_key_file = BytesIO(private_key)
-
-        # Connect to the EC2 instance
-        ssh.connect('44.202.105.110', username='ec2-user', pkey=paramiko.RSAKey(file_obj=private_key_file))
-
-        # Create an SFTP client
-        sftp = ssh.open_sftp()
-
-        # Iterate over files in the remote directory
-        remote_path = '/home/ec2-user/source_dir/'
-        for filename in sftp.listdir(remote_path):
-            remote_file_path = os.path.join(remote_path, filename)
-            local_file_path = '/tmp/' + filename
-            sftp.get(remote_file_path, local_file_path)
-
-            # Upload the file to S3
-            s3_client = boto3.client('s3')
-            s3_key = s3_key_prefix + filename
-            s3_client.upload_file(local_file_path, bucket_name, s3_key)
-
-            # Remove the local file
-            os.remove(local_file_path)
-
-        return {
-            'statusCode': 200,
-            'body': 'Files transferred to S3 successfully'
-        }
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': str(e)
-        }
-    finally:
-        # Close the SFTP and SSH connections
-        sftp.close()
-        ssh.close()
+    return {
+        'statusCode': 200,
+        'body': 'Files transferred from SFTP server'
+    }
